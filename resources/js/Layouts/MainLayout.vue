2de47a70d1f477fce8166556e14d7dd66a22d560
Elvan Hidayat <elvanhd@gmail.com>
2023-06-22 04:03:34 +0700

    displaying flash messages

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,24 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+    <div>{{ y }}</div>
     <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3';
+import { Link, usePage } from '@inertiajs/inertia-vue3';
+import { computed } from 'vue';
+
+const page = usePage();
+const flashSuccess = computed(() => page.props.value.flash.success);
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
