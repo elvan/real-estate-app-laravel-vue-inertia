902f2a89991c57029a7eaabc8510fd133f08cbd7
Elvan Hidayat <elvanhd@gmail.com>
2023-06-22 02:52:32 +0700

    display listings data

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-for="listing in listings" :key="listing.id">
+        <Link :href="`/listing/${listing.id}`">
+            <ListingAddress :listing="listing" />
+        </Link>
+    </div>
+</template>
+
+<script setup>
+import ListingAddress from '@/Components/ListingAddress.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+defineProps({
+    listings: Array,
+});
+</script>
