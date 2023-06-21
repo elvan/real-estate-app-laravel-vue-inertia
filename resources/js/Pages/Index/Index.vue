7d906fbe88a53a148a1aa13c7b7d11b11b289ef6
Elvan Hidayat <elvanhd@gmail.com>
2023-06-21 22:49:01 +0700

    default layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,10 @@
 <template>
-    <MainLayout>
-        <div>Index</div>
-        <Link href="/hello">Show Page</Link>
-        <div>The message is {{ message }}</div>
-    </MainLayout>
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
+    <div>The message is {{ message }}</div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
-import MainLayout from '../../Layouts/MainLayout.vue';
 defineProps({ message: String });
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,8 @@
 <template>
-    <MainLayout>
-        <div>Show</div>
-        <Link href="/">Main Page</Link>
-    </MainLayout>
+    <div>Show</div>
+    <Link href="/">Main Page</Link>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
-import MainLayout from '../../Layouts/MainLayout.vue';
 </script>
