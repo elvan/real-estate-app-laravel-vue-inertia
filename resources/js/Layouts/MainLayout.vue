11eac4a64538f20fa1325c4693e713ac3043e252
Elvan Hidayat <elvanhd@gmail.com>
2023-06-22 16:51:49 +0700

    add basic styling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,10 +1,33 @@
 <template>
-    <Link href="/listing">Listings</Link>&nbsp;
-    <Link href="/listing/create">New Listing</Link>
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
-    <slot>Default</slot>
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.index')">Listings</Link>
+                </div>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href="route('listing.index')">RealEstate</Link>
+                </div>
+                <div>
+                    <Link
+                        :href="route('listing.create')"
+                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md"
+                        >+ New Listing</Link
+                    >
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto p-4">
+        <div
+            v-if="flashSuccess"
+            class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2"
+        >
+            {{ flashSuccess }}
+        </div>
+        <slot>Default</slot>
+    </main>
 </template>
 
 <script setup>
@@ -14,10 +37,3 @@ import { computed } from 'vue';
 const page = usePage();
 const flashSuccess = computed(() => page.props.value.flash.success);
 </script>
-
-<style scoped>
-.success {
-    background-color: green;
-    color: white;
-}
-</style>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>RealEstateApp</title>
+    <title>RealEstate</title>
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
 
-<body class="bg-white dark:bg-gray-900">
+<body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
     @inertia
 </body>
 
