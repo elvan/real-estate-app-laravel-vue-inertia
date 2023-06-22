15a380498b09487451909f0f4a344c7c0612ea1f
Elvan Hidayat <elvanhd@gmail.com>
2023-06-22 22:47:10 +0700

    payment calculator markup

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -12,8 +12,31 @@
             </Box>
 
             <Box>
-                <template #header> Offer </template>
-                Make an offer
+                <template #header> Monthly Payment </template>
+                <div>
+                    <label class="label">Interest rate (2.5%)</label>
+                    <input
+                        type="range"
+                        min="0.1"
+                        max="30"
+                        step="0.1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+
+                    <label class="label">Duration (25 years)</label>
+                    <input
+                        type="range"
+                        min="3"
+                        max="35"
+                        step="1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your monthly payment</div>
+                        <Price :price="500" class="text-3xl" />
+                    </div>
+                </div>
             </Box>
         </div>
     </div>
@@ -23,7 +46,7 @@
 import ListingAddress from '@/Components/ListingAddress.vue';
 import ListingSpace from '@/Components/ListingSpace.vue';
 import Price from '@/Components/Price.vue';
-import Box from '../../Components/UI/Box.vue';
+import Box from '@/Components/UI/Box.vue';
 defineProps({
     listing: Object,
 });
