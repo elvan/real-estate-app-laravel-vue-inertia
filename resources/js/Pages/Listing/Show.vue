1cfc404b6d40819193a463683750ee53b3961115
Elvan Hidayat <elvanhd@gmail.com>
2023-06-22 23:02:09 +0700

    monthly-payment composable

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -50,7 +50,9 @@ import ListingSpace from '@/Components/ListingSpace.vue';
 import Price from '@/Components/Price.vue';
 import Box from '@/Components/UI/Box.vue';
 
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
+
+import { useMonthlyPayment } from '@/Composables/useMonthlyPayment';
 
 const interestRate = ref(2.5);
 const duration = ref(25);
@@ -59,14 +61,5 @@ const props = defineProps({
     listing: Object,
 });
 
-const monthlyPayment = computed(() => {
-    const principle = props.listing.price;
-    const monthlyInterest = interestRate.value / 100 / 12;
-    const numberOfPaymentMonths = duration.value * 12;
-
-    return (
-        (principle * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) /
-        (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
-    );
-});
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration);
 </script>
