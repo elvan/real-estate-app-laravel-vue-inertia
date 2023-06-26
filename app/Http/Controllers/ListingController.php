bcad35f1f57946d3d357333d14660b7de66da99f
Elvan Hidayat <elvanhd@gmail.com>
2023-06-26 23:56:39 +0700

    filtering using local query scope

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -29,25 +29,9 @@ class ListingController extends Controller
             [
                 'filters' => $filters,
                 'listings' => Listing::mostRecent()
-                    ->when(
-                        $filters['priceFrom'] ?? false,
-                        fn ($query, $value) => $query->where('price', '>=', $value)
-                    )->when(
-                        $filters['priceTo'] ?? false,
-                        fn ($query, $value) => $query->where('price', '<=', $value)
-                    )->when(
-                        $filters['beds'] ?? false,
-                        fn ($query, $value) => $query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
-                    )->when(
-                        $filters['baths'] ?? false,
-                        fn ($query, $value) => $query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
-                    )->when(
-                        $filters['areaFrom'] ?? false,
-                        fn ($query, $value) => $query->where('area', '>=', $value)
-                    )->when(
-                        $filters['areaTo'] ?? false,
-                        fn ($query, $value) => $query->where('area', '<=', $value)
-                    )->paginate(10)->withQueryString()
+                    ->filter($filters)
+                    ->paginate(10)
+                    ->withQueryString()
             ]
         );
     }
