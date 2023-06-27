9bbce009359865e59af6cf47556c4bc60ae5b470
Elvan Hidayat <elvanhd@gmail.com>
2023-06-28 00:06:22 +0700

    edit and delete a listing

--- resources/js/Pages/Listing/Index/Components/Listing.vue
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -10,9 +10,6 @@
                 <ListingAddress :listing="listing" class="text-gray-500" />
             </Link>
         </div>
-        <div>
-            <Link :href="route('listing.edit', { listing: listing.id })"> Edit </Link>
-        </div>
     </Box>
 </template>
 
