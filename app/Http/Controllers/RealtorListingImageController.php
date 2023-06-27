985b66fd93f4292ffd3e21f8d6e1dbd6774050a9
Elvan Hidayat <elvanhd@gmail.com>
2023-06-28 06:07:42 +0700

    preparing for file uploads

---
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class RealtorListingImageController extends Controller
+{
+    public function create(Listing $listing)
+    {
+        return inertia(
+            'Realtor/ListingImage/Create',
+            ['listing' => $listing]
+        );
+    }
+
+    public function store(Listing $listing, Request $request)
+    {
+        dd('Works!');
+    }
+}
