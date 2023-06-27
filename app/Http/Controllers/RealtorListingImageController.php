b11d4be0823b85e23d31632402c7feafd7c803a4
Elvan Hidayat <elvanhd@gmail.com>
2023-06-28 06:16:48 +0700

    uploading images

--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Listing;
+use App\Models\ListingImage;
 use Illuminate\Http\Request;
 
 class RealtorListingImageController extends Controller
@@ -18,6 +19,16 @@ class RealtorListingImageController extends Controller
 
     public function store(Listing $listing, Request $request)
     {
-        dd('Works!');
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $file) {
+                $path = $file->store('images', 'public');
+
+                $listing->images()->save(new ListingImage([
+                    'filename' => $path
+                ]));
+            }
+        }
+
+        return redirect()->back()->with('success', 'Images uploaded!');
     }
 }
