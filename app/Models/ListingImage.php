0e88edb4d6ae9ef852dee16cbf32eb10a38f1edb
Elvan Hidayat <elvanhd@gmail.com>
2023-06-28 05:05:29 +0700

    file data model

---
+++ app/Models/ListingImage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Listing;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ListingImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['filename'];
+
+    public function listing(): BelongsTo
+    {
+        return $this->belongsTo(Listing::class);
+    }
+}
