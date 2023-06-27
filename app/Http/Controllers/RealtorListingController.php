35169b4051ae6f3d7aeacb7352a6331fc3d92dba
Elvan Hidayat <elvanhd@gmail.com>
2023-06-27 14:23:59 +0700

    soft deleting listings

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Listing;
 use Illuminate\Support\Facades\Auth;
 
 class RealtorListingController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Listing::class, 'listing');
+    }
+
     public function index()
     {
         return inertia(
             'Realtor/Index',
             ['listings' => Auth::user()->listings]
         );
+    }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->deleteOrFail();
+
+        return redirect()->back()
+            ->with('success', 'Listing was deleted!');
     }
 }
--- resources/js/Pages/Listing/Index/Components/Listing.vue
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -13,9 +13,6 @@
         <div>
             <Link :href="route('listing.edit', { listing: listing.id })"> Edit </Link>
         </div>
-        <div>
-            <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button"> Delete </Link>
-        </div>
     </Box>
 </template>
 
