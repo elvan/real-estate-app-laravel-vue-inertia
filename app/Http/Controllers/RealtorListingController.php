ee3e78ed9860b2689f372f98acf9a8e2f9616ec3
Elvan Hidayat <elvanhd@gmail.com>
2023-06-27 14:04:36 +0700

    realtor's listing

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class RealtorListingController extends Controller
 {
     public function index()
     {
-        return inertia('Realtor/Index');
+        return inertia(
+            'Realtor/Index',
+            ['listings' => Auth::user()->listings]
+        );
     }
 }
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -6,10 +6,10 @@
                     <Link :href="route('listing.index')">Listings</Link>
                 </div>
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
-                    <Link :href="route('listing.index')">RealEstate</Link>
+                    <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
                 <div v-if="user" class="flex items-center gap-4">
-                    <div class="text-sm text-gray-500">{{ user.name }}</div>
+                    <Link class="text-sm text-gray-500" :href="route('realtor.listing.index')">{{ user.name }}</Link>
                     <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
                     <div>
                         <Link :href="route('logout')" method="delete" as="button">Logout</Link>
