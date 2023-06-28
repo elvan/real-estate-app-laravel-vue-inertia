bd53ea9f19f95d91b21fc539087e7b1c59f2604a
Elvan Hidayat <elvanhd@gmail.com>
2023-06-28 17:35:29 +0700

    displaying listing images

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -44,6 +44,8 @@ class ListingController extends Controller
      */
     public function show(Listing $listing)
     {
+        $listing->load(['images']);
+
         return inertia(
             'Listing/Show',
             [
