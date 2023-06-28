562077f53ebac52535daaff0cfb4455177b61f21
Elvan Hidayat <elvanhd@gmail.com>
2023-06-28 23:28:48 +0700

    which offer is sold

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -8,23 +8,19 @@
             <div class="w-full text-center font-medium text-gray-500">No offers</div>
         </Box>
 
-        <div v-else class="md:col-span-7 items-center">
-            <Offer
-                v-for="offer in listing.offers"
-                :key="offer.id"
-                class="mb-4"
-                :offer="offer"
-                :listing-price="listing.price"
-            />
+        <div v-else class="md:col-span-7 flex flex-col gap-4">
+            <Offer v-for="offer in listing.offers" :key="offer.id" :offer="offer" :listing-price="listing.price" />
         </div>
 
-        <Box class="md:col-span-5">
-            <template #header>Basic Info</template>
-            <Price :price="listing.price" class="text-2xl font-bold" />
+        <div class="md:col-span-5">
+            <Box>
+                <template #header>Basic Info</template>
+                <Price :price="listing.price" class="text-2xl font-bold" />
 
-            <ListingSpace :listing="listing" class="text-lg" />
-            <ListingAddress :listing="listing" class="text-gray-500" />
-        </Box>
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />
+            </Box>
+        </div>
     </section>
 </template>
 
--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,6 +1,13 @@
 <template>
     <Box>
-        <template #header>Offer #{{ offer.id }}</template>
+        <template #header
+            >Offer #{{ offer.id }}
+            <span
+                v-if="offer.accepted_at"
+                class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1"
+                >accepeted</span
+            ></template
+        >
 
         <section class="flex items-center justify-between">
             <div>
@@ -14,6 +21,7 @@
             </div>
             <div>
                 <Link
+                    v-if="notSold"
                     :href="route('realtor.offer.accept', { offer: offer.id })"
                     class="btn-outline text-xs font-medium"
                     as="button"
@@ -38,4 +46,6 @@ const props = defineProps({
 });
 const difference = computed(() => props.offer.amount - props.listingPrice);
 const madeOn = computed(() => new Date(props.offer.created_at).toDateString());
+
+const notSold = computed(() => !props.offer.accepted_at && !props.offer.rejected_at);
 </script>
